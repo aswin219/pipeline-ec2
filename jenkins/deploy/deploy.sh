5490a736f09e6bfb22d151732593bb24e80d3088
aswin219 <74107290+aswin219@users.noreply.github.com>
2022-04-19 12:04:40 +0530

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -6,4 +6,4 @@ echo $PASS >> /tmp/.auth
 
 scp -o StrictHostKeyChecking=no  /tmp/.auth ec2-user@13.233.137.66:/tmp/.auth
 scp -o StrictHostKeyChecking=no  ./jenkins/deploy/publish ec2-user@13.233.137.66:/tmp/publish
-ssh -o StrictHostKeyChecking=no /opt/id_rsa ec2-user@13.233.137.66 "/tmp/publish"
+ssh -o StrictHostKeyChecking=no  ec2-user@13.233.137.66 "/tmp/publish"
